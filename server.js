fe203b94321e5189830c5b04de6675308bf553c3
gmonish57 <51042403+gmonish57@users.noreply.github.com>
2019-05-31 20:54:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami",(req,res)=>{
+  res.json({"ipaddress":req.headers['x-forwarded-for'].split(",")[0]||req.ip,"language":req.headers['accept-language'],"software":req.headers['user-agent']})
+})
+
 
 
 // listen for requests :)
